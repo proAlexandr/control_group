9dc9fde361f25311f813a102cbd1c8d6f80697d8
Alexandr <s-promakh@ya.ru>
2012-09-02 16:43:13 +0600

    delete .idea and config helper

---
+++ lib/control_group/railtie.rb
@@ -0,0 +1,11 @@
+require "view_helpers"
+
+module ControlGroup
+
+  class Railtie < Rails::Railtie
+    initializer "control_group.view_helpers" do
+      ActionView::Base.send :include, ViewHelpers
+    end
+  end
+
+end
---
+++ lib/control_group/view_helpers.rb
@@ -0,0 +1,30 @@
+module ControlGroup
+
+  module ViewHelpers
+
+    def control_group f, field_name, input_type = nil, *options, &block
+      help = nil #options.is_a?(Hash) ? options.delete(:help) : nil TODO: Fix it
+      label_field_name = [field_name].flatten.first
+      input_field_name = [field_name].flatten.last
+
+      content_tag :div, :class => "control-group" do
+        raw([
+                f.label(label_field_name, :class => "control-label"),
+
+                if block_given?
+                  content_tag :div, :class => "controls", &block
+                else
+                  content_tag(:div, :class => "controls") do
+                    raw([
+                            f.send(input_type, input_field_name, *options),
+                            help && content_tag(:p, help, :class => "help-block")
+                        ].join(" "))
+                  end
+                end,
+            ].join(" "))
+      end
+    end
+
+  end
+
+end
